25a76496d63ea0758392ef1203db9b23bdc2db96
Dean van Niekerk <deanvniekerk@gmail.com>
2018-04-27 21:52:44 +0200

    export multiple services

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 
-let PortfolioChartService = require('./services/portfolio-chart/portfolio-chart-service');
 
-module.exports = PortfolioChartService;
+let PortfolioChartService = require('./services/portfolio-chart/portfolio-chart-service');
+let TransactionSummaryService = require('./services/transaction-summary/transaction-summary-service');
+let UtilityService = require('./services/utility/utility-service');
+
+module.exports = {
+    PortfolioChartService,
+    TransactionSummaryService,
+    UtilityService
+};
